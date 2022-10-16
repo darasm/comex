20ecbb8c814d5d882057ae3110ae669856b76863
darasm <dara_souza.12@hotmail.com>
2022-10-16 09:53:21 -0300

    [TEST] - save cotegory

--- src/main/java/com/bootcamp/comex/entrypoints/controller/dto/CategoryDto.java
+++ src/main/java/com/bootcamp/comex/entrypoints/controller/dto/CategoryDto.java
@@ -4,8 +4,10 @@ import com.bootcamp.comex.enums.CategoryStatus;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 public class CategoryDto {
---
+++ src/test/java/com/bootcamp/comex/entrypoints/controller/CategoryControllerTest.java
@@ -0,0 +1,44 @@
+package com.bootcamp.comex.entrypoints.controller;
+
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.net.URI;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class CategoryControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Test
+    public void shouldSaveCategory() throws Exception {
+        URI uri = new URI("/api/v1/categories");
+
+        JSONObject jsonObject = new JSONObject().put("name", "Book");
+        String request = jsonObject.toString();
+
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post(uri)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request))
+                .andExpect(status().isCreated());
+    }
+}
